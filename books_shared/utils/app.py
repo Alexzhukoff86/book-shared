bbb29de4fc41c92679982f1686e4d7f313643b59
alex <alexzhukoff86@gmail.com>
2023-05-24 22:36:34 +0300

    Try to fix python_version

--- books_shared/utils/app.py
+++ books_shared/utils/app.py
@@ -4,14 +4,11 @@ from books_shared.utils import logger
 from books_shared.utils.db import db
 app = Flask(__name__)
 
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///account.db'
 
-
-def create_app(config):
-    logger.info(f"Create new app with config {config}")
-    app.config.from_envvar(config)
-    db.init_app(app)
+def create_app():
+    logger.info(f"Create new app")
+    #app.config.from_envvar(config)
+    db.init_app()
     with app.app_context():
         db.create_all()
     return app

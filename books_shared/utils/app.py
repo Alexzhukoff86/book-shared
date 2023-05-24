8f054f6dbdae51e0af86c3c60601aaf14504f2a4
alex <alexzhukoff86@gmail.com>
2023-05-24 22:44:34 +0300

    Try to fix python_version

--- books_shared/utils/app.py
+++ books_shared/utils/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def create_app():
     logger.info(f"Create new app")
     #app.config.from_envvar(config)
-    db.init_app()
+    db.init_app(app)
     with app.app_context():
         db.create_all()
     return app
